ccd042b2c5b7be86b96964c6b4e62fb07c95cd82
kwikclaims <zgreerlennon@gmail.com>
2025-08-31 11:37:59 -0400

    Updated imageRegistry.ts

--- src/data/imageRegistry.ts
+++ src/data/imageRegistry.ts
@@ -44,7 +44,6 @@ export const projectImages = {
     "/images/berry7.jpg",
     "/images/berry8.jpg"
   ],
-
   "5324 Kepler Rd, Temple Hills, MD 20748": [
     "/images/ruth1.jpg",
     "/images/ruth2.jpg",
@@ -57,7 +56,6 @@ export const projectImages = {
     "/images/ruth9.jpg",
     "/images/ruth10.jpg"
   ],
-
   "7610 Martha St, District Heights, MD 20747": [
     "/images/erin1.jpg",
     "/images/erin2.jpg",
@@ -66,7 +64,6 @@ export const projectImages = {
     "/images/erin5.jpg",
     "/images/erin6.jpg"
   ],
-
   "8137 Murray Hill Dr, Fort Washington, MD 20744": [
     "/images/akers1.jpg",
     "/images/akers2.jpg",
@@ -74,79 +71,10 @@ export const projectImages = {
     "/images/akers4.jpg",
     "/images/akers5.jpg"
   ],
-
-  "5911 Center Dr, Temple Hills, MD 20748": [
-    "/images/theresa1.jpg",
-    "/images/theresa2.jpg",
-    "/images/theresa3.jpg",
-    "/images/theresa4.jpg",
-    "/images/theresa5.jpg",
-    "/images/theresa6.jpg",
-    "/images/theresa7.jpg",
-    "/images/theresa8.jpg",
-    "/images/theresa9.jpg",
-    "/images/theresa10.jpg",
-    "/images/theresa11.jpg",
-    "/images/theresa12.jpg",
-    "/images/theresa13.jpg",
-    "/images/theresa14.jpg",
-    "/images/theresa15.jpg",
-    "/images/theresa16.jpg",
-    "/images/theresa17.jpg"
-  ],
-
-  "909 Shelby Dr, Oxon Hill, MD 20745": [
-    "/images/joyce1.jpg",
-    "/images/joyce2.jpg",
-    "/images/joyce3.jpg",
-    "/images/joyce4.jpg"
-  ],
-
-  "13213 Old Chapel Rd, Bowie, MD 20720": [
-    "/images/william1.jpg",
-    "/images/william2.jpg",
-    "/images/william3.jpg",
-    "/images/william4.jpg",
-    "/images/william5.jpg"
-  ],
-
-  "8504 Denton Ct, Clinton, MD 20735": [
-    "/images/wynn1.jpg",
-    "/images/wynn2.jpg",
-    "/images/wynn3.jpg",
-    "/images/wynn4.jpg",
-    "/images/wynn5.jpg",
-    "/images/wynn6.jpg",
-    "/images/wynn7.jpg",
-    "/images/wynn8.jpg",
-    "/images/wynn9.jpg",
-    "/images/wynn10.jpg",
-    "/images/wynn11.jpg",
-    "/images/wynn12.jpg"
-  ],
-
-  "5200 Varnum St, Bladensburg, MD 20710": [
-    "/images/leos1.jpg",
-    "/images/leos2.jpg",
-    "/images/leos3.jpg",
-    "/images/leos4.jpg",
-    "/images/leos5.jpg",
-    "/images/leos6.jpg",
-    "/images/leos7.jpg",
-    "/images/leos8.jpg",
-    "/images/leos9.jpg"
-  ],
-
-  "8027 Carey Branch Pl, Fort Washington, MD 20744": [
-    "/images/cesar1.jpg",
-    "/images/cesar2.jpg",
-    "/images/cesar3.jpg",
-    "/images/cesar4.jpg",
-    "/images/cesar5.jpg",
-    "/images/cesar6.jpg",
-    "/images/cesar7.jpg",
-    "/images/cesar8.jpg",
-    "/images/cesar9.jpg",
-    "/images/cesar10.jpg"
-  ]
+  "5911 Center Dr, Temple Hills, MD 20748": [],
+  "909 Shelby Dr, Oxon Hill, MD 20745": [],
+  "13213 Old Chapel Rd, Bowie, MD 20720": [],
+  "8504 Denton Ct, Clinton, MD 20735": [],
+  "5200 Varnum St, Bladensburg, MD 20710": [],
+  "8027 Carey Branch Pl, Fort Washington, MD 20744": []
 } as const;
